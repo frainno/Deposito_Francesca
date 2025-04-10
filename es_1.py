014044583bed8e0c82e64ee4c2092f7710f13b93
frainno <francesca.innocente25@hotmail.it>
2025-04-10 11:29:23 +0200

    Update es_1.py

--- es_1.py
+++ es_1.py
@@ -2,18 +2,18 @@ import numpy as np
 
 #crea array di 12 num equidistanti tra 0 e 1
 arr=np.linspace(0,1,12)
-print=(arr)
+print(arr)
 
 #cambia forma a una matrce 3x4
 matrice_1=arr.reshape(3,4)
-print=(matrice_1)
+print(matrice_1)
 
 #genera matrice di num casuali tra 0 e 1 
 matrice_2=np.random.rand(3,4)
-print=(matrice_2)
+print(matrice_2)
 
 #somma degli elementi
 somma_matrice_1=np.sum(matrice_1)
 somma_matrice_2=np.sum(matrice_2)
-print=(somma_matrice_1)
-print=(somma_matrice_2)
+print(somma_matrice_1)
+print(somma_matrice_2)
